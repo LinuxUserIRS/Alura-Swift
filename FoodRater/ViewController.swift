985c8305edc8869cd1b9d44de0555e54e03f99ed
Viitra 2 <viitra2@mac-mini-de-viitra-3.local>
2018-10-16 15:28:07 -0300

    Começando o controller.

--- FoodRater/ViewController.swift
+++ FoodRater/ViewController.swift
@@ -20,6 +20,13 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    //Swift força todas as variavéis a terem um valor inicial. Essa variavél do campo de texto só vai ter valor em tempo de execução, mas não de compilação. Então colocamos uma exclamação pra dizer ao compilador que ela é opcional. Dizemos ao compilador para confiar no programador.
+    @IBOutlet var nameField:UITextField!
+    @IBOutlet var happinessField:UITextField!
+    @IBAction func addFood(){
+        let name=nameField.text
+        let happiness=happinessField.text
+        print("Eu comi \(name) e minha felicidade foi \(happiness)");
+    }
 }
 
